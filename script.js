def522116fc871e466af8874ae1a9d85cdab4c27
jamiecorky <jamiecorkindale@gmail.com>
2021-06-25 14:57:52 +0100

    Mike's update to main branch
    added strings and working functions

--- script.js
+++ script.js
@@ -1,3 +1,16 @@
-const randomNumbergenerator = (number) => {
-    let randomNumber = Math.floor(Math.random() * number)
+const getRandomInt = (min, max) => {
+    return Math.floor(Math.random() * (max - min)) + min;
+}
+
+const newFirstName = ['brown', 'bored', 'testy', 'squealing', 'historical', 'befitting', 'busy', 'creepy', 'adamant', 'green', 'incompetent', 'elastic', 'healthy', 'simple', 'ill', 'jazzy', 'flowery', 'deadpan', 'precious', 'mighty', 'loving', 'efficient', 'fresh', 'spooky', 'sparkling', 'quaint', 'pleasant', 'gamy', 'cynical', 'godly', 'puzzled', 'mammoth', 'detailed', 'ignorant', 'chubby', 'polite', 'sad', 'penitent', 'filthy', 'sturdy', 'hard', 'sleepy', 'woozy', 'nice', 'happy', 'enchanted', 'misty', 'nine', 'chilly', 'illustrious', 'fearful', 'obedient', 'physical', 'grubby', 'overwrought', 'weird', 'hushed', 'humdrum', 'upbeat', 'panicky', 'long-term', 'plant', 'alive', 'sneaky', 'conscious', 'scandalous',  'organic', 'breezy', 'forgetful', 'defiant', 'smart', 'hallowed', 'lamentable', 'cuddly', 'crowded', 'picayune', 'hulking', 'whole', 'goofy', 'ruthless', 'last', 'windy', 'brash', 'safe', 'woebegone', 'anxious', 'gentle', 'witty', 'brainy', 'rough', 'wandering', 'faded', 'charming', 'ossified', 'inexpensive', 'decorous', 'ablaze', 'wet', 'nippy', 'full']
+const newLastName = ['stamp', 'minister', 'horse', 'writer', 'pin', 'range', 'glass', 'plant', 'look', 'bead', 'porter', 'fear', 'dogs', 'heat', 'smell', 'home', 'digestion', 'skin', 'cloth', 'wood', 'reward', 'zipper', 'road', 'error', 'middle', 'floor', 'cattle', 'turn', 'current', 'use', 'chess', 'dad', 'anger', 'property', 'club', 'crook', 'magic', 'tree', 'muscle', 'doctor', 'silver', 'drink', 'walk', 'machine', 'marble', 'slip', 'loaf', 'oven', 'cellar', 'volleyball', 'meal', 'system', 'brother', 'lunchroom', 'eggnog', 'oranges', 'stage', 'fire', 'shame', 'chicken', 'worm', 'deer', 'treatment', 'clover', 'screw', 'shop', 'metal', 'summer', 'honey', 'crown', 'hat', 'ring', 'engine', 'touch', 'pies', 'scent', 'eyes', 'stocking', 'knee', 'society', 'crib', 'observation', 'crowd', 'beds', 'servant', 'punishment', 'reaction', 'tub', 'water', 'destruction', 'balance', 'egg', 'earthquake', 'cactus', 'box', 'bone', 'partner', 'structure', 'doll', 'grandfather']
+const newBirthMonth = ['Bangkok', 'Chicago', 'Guatemala City', 'Hangzhou', 'Lapland', 'Timboktu', 'Hanoi', 'Santiago', 'Kharkiv', 'Tijuana', 'Osaka', 'Minsk']
+
+const newName = (firstName, lastName, birthMonth) => {
+    let nameString = [];
+    for (let i = 0; i<=getRandomInt; (0, newFirstName.length))
+    
+    console.log(random.newFirstName)
+    console.log(random.newLastName)
+    console.log(random.newBirthMonth)
 }
